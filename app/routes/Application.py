48c733e60c9993f02839d522432006ebb1214302
Javierolo88 <jjaviermc@misena.edu.co>
2021-09-22 13:39:22 -0500

    se agrego el metodo get de forma individual y grupal

--- app/routes/Application.py
+++ app/routes/Application.py
@@ -3,18 +3,22 @@ from app import app, db
 from app.controllers.Controller import Controller
 
 class Application:
+    ct = Controller()
     @app.route('/')
     def index():
         return 'Api back'
 
     @app.route('/roles')
     def get_roles_all():
-        ct = Controller()
-        roles = ct.get_rol_all()
+        roles = self.ct.get_rol_all()
         return jsonify({'list':roles})
 
     @app.route('/roles/<id>')
     def get_role(id):
-        ct = Controller()
-        roles = ct.get_rol(id)
+        roles = self.ct.get_rol(id)
+        return jsonify({'list':roles})
+
+    @app.route('/roles', methods=['POST'])
+    def add_roles():
+        roles = self.ct.get_rol(id)
         return jsonify({'list':roles})
