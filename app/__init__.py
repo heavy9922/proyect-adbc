1962e51795c16a9555a68061fc1b438eb456a224
Yeferson castiblanco <yefersoncasti@gmail.com>
2024-04-13 20:35:37 -0500

    fix change linter

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,10 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://postgres:Skills2020@localhost/pythondb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://inlaze:inlaze2024@localhost/pythondb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 db = SQLAlchemy(app)
-
-print(datetime.today())
 
 from app.routes.Rol import Application
 from app.routes.Acounts import AccountRoute
